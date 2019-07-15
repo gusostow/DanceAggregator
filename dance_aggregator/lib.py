5a164ca3ede8f64a110a423cc866781e24aaaa7b
Augustus Ostow <ostowster@gmail.com>
2019-07-14 22:53:00 -0400

    Fix gibney

--- dance_aggregator/lib.py
+++ dance_aggregator/lib.py
@@ -6,11 +6,10 @@ from typing import List, Optional
 @dataclass
 class Event:
     title: str
+    instructor: str
     studio: str
-    date: date
     start_time: time
     end_time: time
-    price: str
     url: str
     location: Optional[str] = None
 
@@ -19,22 +18,5 @@ class DanceStudioScraper:
     def __init__(self, studio_name: str):
         self.studio_name = studio_name
 
-    def scrape_date(self, target_date: date) -> List[Event]:
+    def get_events(self) -> List[Event]:
         raise NotImplementedError
-
-    def get_events(
-        self, start_date: Optional[date] = None, n_days: int = 14
-    ) -> List[Event]:
-        if start_date is None:
-            start_date = date.today()
-        target_dates = [
-            start_date + timedelta(days=offset) for offset in range(n_days + 1)
-        ]
-        events: List[Event] = []
-        for target_date in target_dates:
-            events += self.scrape_date(target_date)
-        self.events = events
-        return events
-
-    def as_records(self) -> List[dict]:
-        return [asdict(event) for event in self.events]
--- dance_aggregator/studios/gibney.py
+++ dance_aggregator/studios/gibney.py
@@ -1,78 +1,61 @@
-from datetime import date, datetime, time
-from typing import List, Tuple
+from typing import List
 
 import bs4
-from bs4.element import ResultSet, Tag
+from bs4.element import Tag
 import requests
 
 from dance_aggregator.lib import DanceStudioScraper, Event
 
 
-def get_event_soups(date: date) -> ResultSet:
-    date_str = date.strftime("%Y-%m-%d")
-
-    url = f"https://gibneydance.org/calendar/{date_str}/"
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(
-            f"Gibney responded with status code {response.status_code} - {response.content}"
-        )
-
-    soup = bs4.BeautifulSoup(response.content, features="html.parser")
-    return soup.find_all(attrs={"class": "type-tribe_events"})
-
-
-def parse_time_range(timerange: str) -> Tuple[time, time]:
-    start, end = timerange.split(" – ")
-    fmt = "%I:%M %p"
-    start_time, end_time = (
-        datetime.strptime(start, fmt).time(),
-        datetime.strptime(end, fmt).time(),
+def make_event_record(studio_name: str, row_soup: Tag) -> Event:
+    title = row_soup.find("span", {"class": "classname"}).text.strip()
+    instructor = row_soup.find("td", {"class": "trainer"}).text.strip()
+    start_time = (
+        row_soup.find("span", attrs={"class": "hc_starttime"})
+        .get("data-datetime")
+        .replace('"', "")
     )
-    return start_time, end_time
-
-
-def make_event_record(studio_name: str, event_date: date, event_soup: Tag) -> Event:
-    title = event_soup.find(
-        name="h2", attrs={"class": "tribe-events-list-event-title"}
-    ).text.strip()
-
-    url = event_soup.a.get("href")
-
-    info_box = event_soup.find(name="div", attrs={"class": "tribe-events-event-meta"})
-
-    time_range: str = info_box.div.text.strip()
-    start_time, end_time = parse_time_range(time_range)
-
-    price: str = info_box.find_all(name="p")[1].text.strip()
-
-    # Need to follow link to get location
-    event_page_soup = bs4.BeautifulSoup(
-        requests.get(url).content, features="html.parser"
+    end_time = (
+        row_soup.find("span", attrs={"class": "hc_endtime"})
+        .get("data-datetime")
+        .replace('"', "")
     )
-    info_paragraphs = event_page_soup.find_all("p")
-    location = info_paragraphs[2].a.text.strip()
+
+    url = row_soup.find("span", {"class": "classname"}).a.get("data-url")
+
+    class_page_soup = bs4.BeautifulSoup(requests.get(url).content)
+    location = class_page_soup.find(
+        "div", {"class": "class_description"}
+    ).span.text.split(": ")[-1]
 
     return Event(
         title=title,
         url=url,
-        date=event_date,
         start_time=start_time,
         end_time=end_time,
-        price=price,
         studio=studio_name,
+        instructor=instructor,
         location=location,
     )
 
 
 class Gibney(DanceStudioScraper):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__(studio_name="Gibney Dance")
+        self.soup = bs4.BeautifulSoup(
+            requests.get(
+                "https://widgets.healcode.com/widgets/schedules/00108315175.json?mobile=false&version=0.1"
+            ).json()["contents"]
+        )
 
-    def scrape_date(self, target_date: date) -> List[Event]:
-        event_soups = get_event_soups(target_date)
-        records = [
-            make_event_record(self.studio_name, target_date, soup)
-            for soup in event_soups
+    def get_events(self) -> List[Event]:
+        event_soups = [
+            soup
+            for soup in self.soup.find_all("tr")
+            if soup.get("class").pop() != "schedule_header"
         ]
-        return records
+
+        events = []
+        for event_row in event_soups:
+            events.append(make_event_record(self.studio_name, event_row))
+        return events
