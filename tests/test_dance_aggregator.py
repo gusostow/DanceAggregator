59efc5df14a888051d9a0ef1109dadc0f0212eb9
Augustus Ostow <ostowster@gmail.com>
2019-08-04 08:55:35 -0400

    Basic scraping integration test

--- tests/test_dance_aggregator.py
+++ tests/test_dance_aggregator.py
@@ -1,5 +1,30 @@
-from dance_aggregator import __version__
+from dance_aggregator.studios import cunningham
+from dance_aggregator.studios import dance_wave
+from dance_aggregator.studios import gibney
+from dance_aggregator.studios import mark_morris
+from dance_aggregator.studios import movement_research
+from dance_aggregator.studios import peridance
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_cunningham():
+    cunningham.Cunningham().get_events()
+
+
+def test_dance_wave():
+    dance_wave.DanceWave().get_events()
+
+
+def test_gibney():
+    gibney.Gibney().get_events()
+
+
+def test_mark_morris():
+    mark_morris.MarkMorris().get_events()
+
+
+def test_movement_research():
+    movement_research.MovementResearch().get_events()
+
+
+def test_peridance():
+    peridance.Peridance().get_events()
